647a37127867cf25c0b70a56834ded4e0b305754
BogdanHorju <bogdanhorju@gmail.com>
2025-09-13 01:22:06 +0200

    Disabilita regola no-explicit-any su button-link

--- src/components/ui/button-link.tsx
+++ src/components/ui/button-link.tsx
@@ -1,6 +1,7 @@
 // components/ui/button-link.tsx
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function ButtonLink({ href, variant, children }: any) {
   return (
     <Link href={href} className={buttonVariants({ variant })}>
