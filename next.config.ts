fdb3e48be3660cd3bfe133f51ba2c014c215aff1
BogdanHorju <bogdanhorju@gmail.com>
2025-09-13 12:15:43 +0200

    Fix type errors and update config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-
 export default {
   eslint: { ignoreDuringBuilds: true },
 };
+
+git add .
+git commit -m "Fix type errors and update config"
+git push
